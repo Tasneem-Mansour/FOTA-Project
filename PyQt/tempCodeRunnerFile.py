190990eff993d563a2ea60af0c31a42cd655cf51
Tasneem Mansour <114835445+Tasneem-Mansour@users.noreply.github.com>
2024-04-17 21:02:08 +0200

    style: move .ui files into another folder

--- PyQt/tempCodeRunnerFile.py
+++
@@ -1,9 +0,0 @@
-
-    # def createaccfunction(self):
-    #     email = self.email.text()
-    #     if self.password.text()==self.confirmpass.text():
-    #         password=self.password.text()
-    #         print("Successfully created acc with email: ", email, "and password: ", password)
-    #         login=Login()
-    #         widget.addWidget(login)
-    #         widget.setCurrentIndex(widget.currentIndex()+1)
