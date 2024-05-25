9f55e43c310e0ccf7d8b2e7aac90e6e81e7d6725
Dina Gamal-eldin <116835284+Dina-Gamal-eldin@users.noreply.github.com>
2024-05-25 23:40:33 +0300

    Add files via upload

---
+++ Server/script.js
@@ -0,0 +1,141 @@
+var input = document.getElementById("myfile");
+var output = document.getElementById("output");
+
+var upload_button = document.getElementById("upload_button");
+
+var text = null
+var connection_status = 'NOT CONNECTED'
+
+
+input.addEventListener("change", function () {
+    if (this.files && this.files[0]) {
+        var myFile = this.files[0];
+        var reader = new FileReader();
+
+        reader.addEventListener('load', function (e) {
+            text = e.target.result;
+            example_name.textContent = "";
+            output.textContent = text;
+            console.log("FILE READ SUCCESS")
+
+        });
+
+        reader.readAsBinaryString(myFile);
+    }
+});
+
+
+function upload() {
+
+    if (text != null) {
+        upload_button.className = "fa fa-spinner fa-spin";
+        text_edited = text.replace(/\n|\r/g, " ");
+        var array = text_edited.split(" ");
+        var i = 0;
+        while (i < array.length) {
+            if (array[i] === "") {
+                array.splice(i, 1);
+            } else {
+                ++i;
+            }
+        }
+        console.log("STARTING UPLOAD ....");
+        const http = new EasyHTTP;
+           
+     			http.put('https://fir-led-sing-default-rtdb.asia-southeast1.firebasedatabase.app/line.json', array)
+            	.then(data => {
+            	       const code_data = {
+           				 lines: array.length,
+           				 new: true,
+                         flag: true
+          		};
+                     
+                          
+                http.put('https://fir-led-sing-default-rtdb.asia-southeast1.firebasedatabase.app/code.json', code_data)
+                .then(data => {
+                    upload_button.className = "fa-spin"
+                    alert("Upload Success")
+    
+                })
+                .catch(err => console.log(err));
+
+            })
+            .catch(err => console.log(err));
+
+
+    }
+    else {
+        alert("Please choose file to upload");
+    }
+}
+async function testConnection() {
+    conn_button.className = "fa fa-spinner fa-spin"
+    test('https://fir-led-sing-default-rtdb.asia-southeast1.firebasedatabase.app/connection.json').then(
+        () => {
+            conn_button.className = "fa-spin"
+        }
+    )
+}
+
+
+function test(url) {
+    var result = null;
+    const http = new EasyHTTP;
+    const data = {
+        status: 'NOT CONNECTED',
+    }
+    return new Promise(resolve => {
+        console.log("TESTING .......");
+        http.put(url, data)
+            .then(data => testSent(url).then((value) => {
+                connection_status = value;
+                web_node.textContent = value;
+                if (value == "CONNECTED") {
+                    web_node.className = "w3-tag w3-green w3-margin-left"
+                }
+                else if (value == "NOT CONNECTED") {
+                    web_node.className = "w3-tag w3-red w3-margin-left"
+                }
+                resolve(data);
+            }
+            ))
+            .catch(err => console.log(err));
+    });
+
+}
+
+function testSent(url) {
+    var result = null
+    return new Promise(resolve => {
+        setTimeout(() => {
+            fetch(url)
+                .then(function (response) {
+                    return response.json();
+                })
+                .then(function (myJson) {
+                    result = myJson.status;
+                    console.log(result)
+                    resolve(result);
+                })
+                .catch(function (error) {
+                    console.log("Error: " + error);
+                });
+
+        }, 5000);
+    });
+}
+
+
+class EasyHTTP {
+    async put(url, data) {
+        const response = await fetch(url, {
+            method: 'PUT',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        });
+        const resData = await response.json();
+        return resData;
+    }
+}
