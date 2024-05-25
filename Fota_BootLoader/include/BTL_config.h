a36387a347085c184bd8727f378353f34e8c2258
Yasmeen Mohamed <67623624+yasmeenmohamed420@users.noreply.github.com>
2024-05-25 23:48:00 +0300

    Add files via upload

---
+++ Fota_BootLoader/include/BTL_config.h
@@ -0,0 +1,19 @@
+/*
+ * BTL.config.h
+ *
+ *       Author: Yasmeen
+ */
+
+#ifndef BTL_CONFIG_H
+#define BTL_CONFIG_H
+
+#define BTL_TIMEOUT   8  //15
+
+
+/* To change these values you must change it too in NodeMCU code*/
+#define BTL_DATA_START_CHAR     'X'
+#define BTL_LINE_BREAK_CHAR     'Y'
+#define BTL_DATA_END_CHAR       'Z'
+#define BTL_READ_CONFIRM_CHAR   'R'
+
+#endif
